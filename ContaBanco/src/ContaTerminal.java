096cdafabf4e7b7ceff8c11485772da512f8597f
Namibo06 <namibo654@gmail.com>
2024-04-25 22:08:42 -0300

    build: ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,2 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    public static void main(String[] args){
+        Double saldo=237.48;
+
+        Scanner scanner= new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor,digite seu nome");
+        String nomeCliente=scanner.next();
+
+        System.out.println("Por favor,digite o número da agência");
+        String agencia=scanner.next();
+
+        System.out.println("Por favor,digite o número da conta");
+        int numero=scanner.nextInt();
+
+        System.out.println("Olá "+ nomeCliente + ",obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
+    }
 }
